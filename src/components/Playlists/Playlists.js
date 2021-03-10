f338efb5caedce11140a783c95be5e000d199cc8
bastienxs <bastienxs@gmail.com>
2021-03-10 18:45:39 +0100

    initial layout

---
+++ src/components/Playlists/Playlists.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Playlists.module.css'
+
+const Playlists = () => {
+    return <div className={styles.Playlists}>
+        Playlists
+    </div>
+}
+
+export default Playlists
---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './SideBar.module.css'
+
+const SideBar = () => {
+    return <div className={styles.SideBar}>
+        Sidebar
+    </div>
+}
+
+export default SideBar
