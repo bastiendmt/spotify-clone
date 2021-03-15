6f5766d8c9e4d94e62fcd31d5f7ecdc6a450036a
bastienxs <bastienxs@gmail.com>
2021-03-15 18:01:37 +0100

    ui tweak

--- src/components/Playlists/Playlist/Playlist.js
+++ src/components/Playlists/Playlist/Playlist.js
@@ -5,6 +5,9 @@ const Playlist = (props) => {
         <div className={styles.imgContainer}>
             <img src={props.playlist.images[0].url} alt="Tokyo" className={styles.Img} />
         </div>
+        <div className={styles.PlayContainer}>
+
+        </div>
         <div className={styles.Name}>{props.playlist.name}</div>
         <div className={styles.Artist}>{props.playlist.owner?.display_name}</div>
     </div>
