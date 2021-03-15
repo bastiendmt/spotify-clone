29e750384b8bb88d097265c7ca9c077380d2eb37
bastienxs <bastienxs@gmail.com>
2021-03-15 21:57:58 +0100

    play button on hover

--- src/components/Playlists/Playlist/Playlist.js
+++ src/components/Playlists/Playlist/Playlist.js
@@ -4,9 +4,9 @@ const Playlist = (props) => {
     return <div className={styles.Playlist}>
         <div className={styles.imgContainer}>
             <img src={props.playlist.images[0].url} alt="Tokyo" className={styles.Img} />
-        </div>
-        <div className={styles.PlayContainer}>
-
+            <div className={styles.PlayContainer}>
+                <button className={styles.PlayButton} title="Play" ><svg height="16" role="img" width="16" viewBox="0 0 24 24" aria-hidden="true"><polygon points="21.57 12 5.98 3 5.98 21 21.57 12" fill="currentColor"></polygon></svg></button>
+            </div>
         </div>
         <div className={styles.Name}>{props.playlist.name}</div>
         <div className={styles.Artist}>{props.playlist.owner?.display_name}</div>
