44be6424336045e53d08ad3cc3e76edce9888ec6
bastienxs <bastienxs@gmail.com>
2021-04-06 22:24:59 +0200

    Playbutton

--- src/components/Playlists/Playlist/Playlist.js
+++ src/components/Playlists/Playlist/Playlist.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import PlayButton from "../../../assets/PlayButton";
 import styles from "./Playlist.module.css";
 
 const Playlist = (props) => {
@@ -13,18 +14,7 @@ const Playlist = (props) => {
           />
           <div className={styles.PlayContainer}>
             <button className={styles.PlayButton} title="Play">
-              <svg
-                height="16"
-                role="img"
-                width="16"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <polygon
-                  points="21.57 12 5.98 3 5.98 21 21.57 12"
-                  fill="currentColor"
-                ></polygon>
-              </svg>
+              <PlayButton />
             </button>
           </div>
         </div>
