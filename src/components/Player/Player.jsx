38b641f17e82d39774c89dbab6703b7807ea8c0b
bastienxs <bastienxs@gmail.com>
2021-04-23 19:21:01 +0200

    song info

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { HeartEmpty } from "../../assets/HeartEmpty";
 import styles from './Player.module.css'
 
 const Player = () => {
   return <div className={styles.Player}>
       <footer>
-          <div className={styles.Song}>Song</div>
+          <div className={styles.Song}>
+            <div className={styles.Song_Img}>
+              <img src="https://bigcats.be/images/resized/750x-header-cat.jpg" alt='song' />
+            </div>
+            <div className={styles.Song_Infos}>
+              <div className={styles.Sonf_Infos_Name}>Song</div>
+              <div className={styles.Sonf_Infos_Artist}>Artist</div>
+            </div>
+            <div className={styles.Song_Like}><HeartEmpty /></div>
+            </div>
           <div className={styles.Controls}>Controls</div>
           <div className={styles.Volume}>Volume</div>
       </footer>
