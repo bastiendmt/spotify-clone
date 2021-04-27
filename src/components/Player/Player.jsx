40079ffb65b48bc4ea9068f4200ff0386ac0fb10
bastienxs <bastienxs@gmail.com>
2021-04-27 23:38:49 +0200

    sass integration

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,24 +1,52 @@
 import React from "react";
 import { HeartEmpty } from "../../assets/HeartEmpty";
-import styles from './Player.module.css'
+import styles from "./Player.module.scss";
 
 const Player = () => {
-  return <div className={styles.Player}>
+  return (
+    <div className={styles.Player}>
       <footer>
-          <div className={styles.Song}>
-            <div className={styles.Song_Img}>
-              <img src="https://bigcats.be/images/resized/750x-header-cat.jpg" alt='song' />
-            </div>
-            <div className={styles.Song_Infos}>
-              <div className={styles.Sonf_Infos_Name}>Song</div>
-              <div className={styles.Sonf_Infos_Artist}>Artist</div>
-            </div>
-            <div className={styles.Song_Like}><HeartEmpty /></div>
-            </div>
-          <div className={styles.Controls}>Controls</div>
-          <div className={styles.Volume}>Volume</div>
+        <div className={styles.Song}>
+          <div className={styles.Img}>
+            <img
+              src="https://bigcats.be/images/resized/750x-header-cat.jpg"
+              alt="song"
+            />
+          </div>
+          <div className={styles.Infos}>
+            <div className={styles.Name}>Song</div>
+            <div className={styles.Artist}>Artist</div>
+          </div>
+          <div className={styles.Song_Like}>
+            <HeartEmpty />
+          </div>
+        </div>
+
+        <div className={styles.Controls}>
+          <div>
+            <button>
+              <svg
+                role="img"
+                height="16"
+                width="16"
+                viewBox="0 0 16 16"
+                class="Svg-ulyrgf-0 dIsYZz"
+              >
+                <path d="M4.018 14L14.41 8 4.018 2z"></path>
+              </svg>
+            </button>
+          </div>
+          <div className={styles.Bar}>
+            <hr />
+          </div>
+        </div>
+
+        <div className={styles.Volume}>
+          Volume
+          </div>
       </footer>
-  </div>;
+    </div>
+  );
 };
 
 export default Player;
