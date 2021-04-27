b2d89a346e1c809b2953dde2f12a57e6313605dd
bastienxs <bastienxs@gmail.com>
2021-04-28 00:01:31 +0200

    clean up files

---
+++ src/Assets/Like.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Like = () => {
+  return (
+    <svg
+      role="img"
+      height="16"
+      width="16"
+      viewBox="0 0 16 16"
+      class="Svg-ulyrgf-0 dIsYZz"
+    >
+      <path d="M4.018 14L14.41 8 4.018 2z"></path>
+    </svg>
+  );
+};
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { HeartEmpty } from "../../assets/HeartEmpty";
+import { Like } from "../../assets/Like";
+import { Volume } from "../../assets/Volume";
 import styles from "./Player.module.scss";
 
 const Player = () => {
@@ -25,15 +27,7 @@ const Player = () => {
         <div className={styles.Controls}>
           <div>
             <button>
-              <svg
-                role="img"
-                height="16"
-                width="16"
-                viewBox="0 0 16 16"
-                class="Svg-ulyrgf-0 dIsYZz"
-              >
-                <path d="M4.018 14L14.41 8 4.018 2z"></path>
-              </svg>
+              <Like />
             </button>
           </div>
           <div className={styles.Bar}></div>
@@ -42,17 +36,7 @@ const Player = () => {
         <div className={styles.Volume}>
           <div>
             <button>
-              <svg
-                role="presentation"
-                height="16"
-                width="16"
-                aria-label="Volume high"
-                id="volume-icon"
-                viewBox="0 0 16 16"
-                class="Svg-ulyrgf-0 dIsYZz"
-              >
-                <path d="M12.945 1.379l-.652.763c1.577 1.462 2.57 3.544 2.57 5.858s-.994 4.396-2.57 5.858l.651.763a8.966 8.966 0 00.001-13.242zm-2.272 2.66l-.651.763a4.484 4.484 0 01-.001 6.397l.651.763c1.04-1 1.691-2.404 1.691-3.961s-.65-2.962-1.69-3.962zM0 5v6h2.804L8 14V2L2.804 5H0zm7-1.268v8.536L3.072 10H1V6h2.072L7 3.732z"></path>
-              </svg>
+              <Volume />
             </button>
           </div>
           <div className={styles.VolumeBar}></div>
