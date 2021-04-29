4f6ac31f2efba6420088f791bdc1503aec617dbd
bastienxs <bastienxs@gmail.com>
2021-04-29 14:17:19 +0200

    error message if data not loaded

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from "./App.module.css";
 
 import SideBar from "./components/SideBar/SideBar";
-import Player from './components/Player/Player';
+import Player from "./components/Player/Player";
 
 import Playlists from "./pages/Playlists/Playlists";
 import PlaylistDetail from "./pages/PlaylistDetail/PlaylistDetail";
@@ -13,9 +13,14 @@ import { connect } from "react-redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = ({ playlists, initPlaylists }) => {
+  const [error, setError] = useState();
   const loadPlaylists = useCallback(async () => {
     await GetPlaylists().then((data) => {
-      initPlaylists(data.playlists.items);
+      if (data?.playlists) {
+        initPlaylists(data?.playlists?.items);
+      } else {
+        setError(<div className={styles.Error}>Could not load data</div>);
+      }
     });
   }, [initPlaylists]);
 
@@ -23,23 +28,27 @@ const App = ({ playlists, initPlaylists }) => {
     loadPlaylists();
   }, [loadPlaylists]);
 
-  return (
-    <div className={styles.App}>
-      <Router>
-        {playlists && <SideBar />}
+  if (error) {
+    return error;
+  } else {
+    return (
+      <div className={styles.App}>
+        <Router>
+          {playlists && <SideBar />}
 
-        <Route path="/" exact>
-          {playlists && <Playlists />}
-        </Route>
+          <Route path="/" exact>
+            {playlists && <Playlists />}
+          </Route>
 
-        <Route path="/playlist/:id">
-          <PlaylistDetail />
-        </Route>
+          <Route path="/playlist/:id">
+            <PlaylistDetail />
+          </Route>
 
-        <Player />
-      </Router>
-    </div>
-  );
+          <Player />
+        </Router>
+      </div>
+    );
+  }
 };
 
 const mapStateToProps = (state) => {
