05c7eea3bb65fbb2fbd84740d7dda3a0a3dfc4df
bastienxs <bastienxs@gmail.com>
2021-05-04 18:21:00 +0200

    uppercase folder

---
+++ src/assets/Pause.tsx
@@ -0,0 +1,13 @@
+export const Pause: React.FC = () => {
+  return (
+    <svg
+      role="img"
+      height="16"
+      width="16"
+      viewBox="0 0 16 16"
+    >
+      <path fill="none" d="M0 0h16v16H0z"></path>
+      <path d="M3 2h3v12H3zM10 2h3v12h-3z"></path>
+    </svg>
+  );
+};
