b7ae43d3cede81c371ebbca05c8a2b8eeab0ef17
Bastien <bastienxs@gmail.com>
2022-10-07 22:00:50 +0200

    remove sonar smells

--- src/store/store.ts
+++ src/store/store.ts
@@ -13,4 +13,3 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
-// const store = createStore(reducers, composeEnhancers());
