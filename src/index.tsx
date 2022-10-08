6ade1347f18411befe24637df263cae04dacbfd0
Bastien <bastienxs@gmail.com>
2022-10-08 22:38:31 +0200

    base libs

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.module.scss";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { compose } from "redux";
+import App from "./App";
+import "./index.module.scss";
 import { store } from "./store/store";
-
 declare global {
   interface Window {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
   }
 }
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
