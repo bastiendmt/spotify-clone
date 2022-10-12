ca30ca9b35472b8b28b61ea4fd9de3786829897e
Bastien Dumont <32489032+bastiendmt@users.noreply.github.com>
2022-10-12 17:50:26 +0200

    React scripts 5 (#15)
    * fix undefined param
    
    * fix play button hover effect when disabled
    
    * working without react-sound
    
    * remove comment
    
    * fix build
    
    * fix smell

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from "react";
-import Sound from "react-sound";
 import { Like } from "../../assets/Like";
 import { Pause } from "../../assets/Pause";
 import { Play } from "../../assets/Play";
@@ -9,31 +8,46 @@ import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { playpause } from "../../store/reducers/playing.reducer";
 import { millisToMinutesAndSeconds } from "../../utils/msToMinutes";
 import { useBar } from "../../utils/useBar";
+import useStopwatch from "../../utils/useStopwatch";
 import styles from "./Player.module.scss";
 
 const Player = () => {
-  const [time, setTime] = useState(0);
+  const { song, playing } = useAppSelector((state) => state.playing);
+  const audioEml = useRef<HTMLAudioElement | null>(null);
+  const dispatch = useAppDispatch();
+
   const timeRef = useRef<HTMLDivElement | null>(null);
+  const barCallBack = useBar;
+
   const [progress, setProgress] = useState(0);
+  const [currentTime, resetTime, toggleStopwatch] = useStopwatch();
+
   const [volume, setVolume] = useState(70);
   const volumeRef = useRef<HTMLDivElement | null>(null);
   const [mute, setMute] = useState(false);
 
-  const barCallBack = useBar;
-
-  const { song, playing } = useAppSelector((state) => state.playing);
-  const dispatch = useAppDispatch();
-
+  //If the songs changes, plays it
   useEffect(() => {
-    // Adjust time when progress bar is clicked
-    setTime((progress * 30000) / 100);
-  }, [progress]);
-
-  useEffect(() => {
-    //Reset progress if the song change
-    setProgress(0);
-    setTime(0);
+    audioEml.current?.play();
+    resetTime();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [song]);
+
+  //Handles play / pause
+  useEffect(() => {
+    if (playing) {
+      audioEml.current?.play();
+      toggleStopwatch(true);
+    } else {
+      audioEml.current?.pause();
+      toggleStopwatch(false);
+    }
+  }, [playing, toggleStopwatch]);
+
+  // Adjust progress bar when playing
+  useEffect(() => {
+    setProgress((currentTime * 100) / 30000);
+  }, [currentTime]);
 
   useEffect(() => {
     if (volume < 5) {
@@ -65,13 +79,14 @@ const Player = () => {
             </div>
 
             <div className={styles.Controls}>
+              <audio ref={audioEml} src={song.track.preview_url} />
               <div>
                 <button onClick={() => dispatch(playpause())}>
                   {playing ? <Pause /> : <Play />}
                 </button>
               </div>
               <div className={styles.BarContainer}>
-                <div>{millisToMinutesAndSeconds(time)}</div>
+                <div>{millisToMinutesAndSeconds(currentTime)}</div>
                 <div
                   className={styles.Wrapper}
                   onClick={(event) => barCallBack(event, timeRef, setProgress)}
@@ -112,20 +127,6 @@ const Player = () => {
               </div>
             </div>
           </footer>
-          {song.track.preview_url && (
-            <Sound
-              url={song.track.preview_url}
-              playStatus={playing ? "PLAYING" : "PAUSED"}
-              //@ts-ignore
-              onPlaying={({ position }) => {
-                setTime(position);
-                setProgress((position * 100) / 30000);
-              }}
-              onFinishedPlaying={() => dispatch(playpause())}
-              volume={mute ? 0 : volume}
-              position={time}
-            />
-          )}
         </div>
       )}
     </>
---
+++ src/utils/useStopwatch.ts
@@ -0,0 +1,29 @@
+import { Dispatch, SetStateAction, useState, useEffect } from "react";
+
+/**
+ * Custom stopwatch
+ * @returns [time, resetTime(), setRunning(on/off)]
+ */
+const useStopwatch = (): [
+  number,
+  () => void,
+  Dispatch<SetStateAction<boolean>>
+] => {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+  useEffect(() => {
+    let interval: any;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
+  return [time, () => setTime(0), setRunning];
+};
+
+export default useStopwatch;
