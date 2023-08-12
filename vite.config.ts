5c7da194567334f7a3fb2e55eccbe3e135e5647a
Bastien Dumont <bastienxs@gmail.com>
2023-08-12 15:00:48 +0200

    vite config

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import svgrPlugin from 'vite-plugin-svgr';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: { outDir: 'build' },
+  plugins: [
+    react(),
+    checker({
+      overlay: { initialIsOpen: false },
+      typescript: true,
+    }),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+  ],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api-server/': '...',
+      '/authorization/': '...',
+    },
+  },
+});
