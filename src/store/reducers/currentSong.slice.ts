f7b0e02c5d5d38747cc28ab3bc7ccaee450dd4dd
Bastien Dumont <bastienxs@gmail.com>
2023-09-09 14:28:23 +0200

    organize imports

--- src/store/reducers/currentSong.slice.ts
+++ src/store/reducers/currentSong.slice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { Track } from '../../types/track.interface';
 import { RootState } from '../store';
 
