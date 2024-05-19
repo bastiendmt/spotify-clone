6b62f4e091e96e30df29ddf1e7a464a18ef94c94
Bastien Dumont <bastienxs@gmail.com>
2024-05-19 15:10:18 +0200

    fix module css for index

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import './index.module.scss';
+import './index.scss';
 import { store } from './store/store';
 
 const container = document.getElementById('root');
