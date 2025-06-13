058ad22d8613e1958c4941d8d99c6e1f5c9d8355
Bastien DUMONT <bastienxs@gmail.com>
2025-06-13 23:35:04 +0200

    Add React Compiler

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 /// <reference types="vitest" />
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import checker from 'vite-plugin-checker';
-import svgrPlugin from 'vite-plugin-svgr';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import svgrPlugin from "vite-plugin-svgr";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  build: { outDir: 'build' },
+  build: { outDir: "build" },
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
     checker({
       overlay: { initialIsOpen: false },
       typescript: true,
@@ -20,8 +24,8 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api-server/': '...',
-      '/authorization/': '...',
+      "/api-server/": "...",
+      "/authorization/": "...",
     },
   },
 });
